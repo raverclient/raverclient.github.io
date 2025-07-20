c04cf2037e92a0bdb23393709d733146272c7e4e
raverclient <rasooihoh@proton.me>
2025-07-20 20:13:27 +0000

    Create global.js

---
+++ js/global.js
@@ -0,0 +1,2 @@
+let currentYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = `<p class="text">&copy; ${currentYear} Raver Team. All Rights Reserved.<br>We are not affiliated with Microsoft and/or Mojang.</p>`;
